327edb51d9fb291974d250dc9d7eb0171db7593a
RileyAHall <81394616+RileyAHall@users.noreply.github.com>
2021-04-01 12:07:16 -0500

    Add files via upload

---
+++ Business Card/BusinessCard.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Bricks and Minifigs"<<std::endl;
+	std::cout<<"12415 Bandera Rd Ste 212, Helotes, Tx 78023"<<std::endl;
+	std::cout<<"+12104372985"<<std::endl;
+	std::cout<<"https://bricksandminifigs.com/"<<std::endl;
+	return 0;
+}
