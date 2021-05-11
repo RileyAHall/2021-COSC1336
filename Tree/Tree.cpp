b1e69436b1deeeeee5b8c03da882fefe5f6a56b9
RileyAHall <81394616+RileyAHall@users.noreply.github.com>
2021-05-11 16:43:30 -0500

    Add files via upload

--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -1,14 +1,25 @@
+#include "Tree.h"
 #include <iostream>
 
 int main(){
 
-	int height;
+	int heigh;
 	int spacing;
 	int asterisk;
 
 	std::cout<<"How tall do you want your tree to be:"<<std::endl;
-	std::cin>>height;
-	spacing=height;
+	std::cin>>heigh;
+	spacing=heigh;
+
+	printTree(heigh);
+
+	return 0;
+}
+
+void printTree(int height){
+
+	int spacing=height;
+	int asterisk;
 
 	for(int i=1;i<height;i++){
 
@@ -27,6 +38,4 @@ int main(){
 
 	std::cout<<"*";
 	std::cout<<"\n";
-
-	return 0;
 }
---
+++ Tree/Tree.h
@@ -0,0 +1,4 @@
+#ifndef _tree_h_
+#define _tree_h_
+void printTree(int height);
+#endif
